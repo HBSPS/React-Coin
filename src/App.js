3f39b32ea04d34a39c5ed4428fea1f822ab79f52
HBSPS <wiyeong79@naver.com>
2022-08-07 13:31:25 +0900

    :sparkles: Extending Components
    컴포넌트의 확장과 props의 사용

--- src/App.js
+++ src/App.js
@@ -4,29 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
-
-const Text = styled.h1`
-  color: white;
+const Circle = styled(Box)`
+  border-radius: 50%;
 `;
 
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal"/>
+      <Circle bgColor="tomato"/>
     </Father>
   )
 };
