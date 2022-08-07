ab2d12ac1c2b6d282a1dd3a5b0b193b3d7316bc4
HBSPS <wiyeong79@naver.com>
2022-08-08 00:06:59 +0900

    :sparkles: Themes
    Styled-Components와 TS를 이용해 다크모드/라이트모드 간단 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    const { currentTarget: { value } } = event;
-    setValue(value);
-  };
-
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("Hello", value);
-  };
-  
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input value={value} onChange={onChange} type="text" placeholder="userName" />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Hello World!</H1>
+    </Container>
   )
 };
 
