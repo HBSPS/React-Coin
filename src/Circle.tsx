d285e6c171a6f5af155aba53bb642ed8e4ab4fa9
HBSPS <wiyeong79@naver.com>
2022-08-07 22:56:58 +0900

    :sparkles: Optional Props
    Optional 적용과 ??를 통한 Default 값의 지정

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,17 +2,27 @@ import styled from "styled-components"
 
 interface CircleProps {
     bgColor: string;
+    borderColor?: string;
+    text?: string;
 }
 
-const Container = styled.div<CircleProps>`
+interface ContainerProps {
+    bgColor: string;
+    borderColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
     width: 200px;
     height: 200px;
     background-color: ${(props) => props.bgColor};
     border-radius: 50%;
+    border: 1px solid ${(props) => props.borderColor};
 `;
 
-function Circle({ bgColor }: CircleProps) {
-    return <Container bgColor={ bgColor } />;
+function Circle({ bgColor, borderColor, text = "Default Text" }: CircleProps) {
+    return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+        {text}
+    </Container>;
 };
 
 export default Circle;
