d1aa94f8c4613f851c531ace13456a96a93e4cc9
HBSPS <wiyeong79@naver.com>
2022-08-07 13:56:30 +0900

    :sparkles: as & attrs
    as를 이용한 태그 설정 방법과 attrs를 이용한 속성 설정 방법

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50%;
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
 `;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="teal"/>
-      <Circle bgColor="tomato"/>
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
   )
 };
