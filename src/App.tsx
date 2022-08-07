d01981fd04d2294be260de1596df18626e93e66a
HBSPS <wiyeong79@naver.com>
2022-08-07 23:37:08 +0900

    :sparkles: Form with React + TypeScript
    React와 TS를 이용해 form 만들기 + useState의 보호

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import Circle from "./Circle";
+import React, { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState("");
+
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const { currentTarget: { value } } = event;
+    setValue(value);
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Hello", value);
+  };
+  
   return (
     <div>
-      <Circle borderColor="black" bgColor="teal" />
-      <Circle text="This is Text" bgColor="tomato" />
+      <form onSubmit={onSubmit}>
+        <input value={value} onChange={onChange} type="text" placeholder="userName" />
+        <button>Log in</button>
+      </form>
     </div>
   )
 };
