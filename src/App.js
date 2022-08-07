fc6070ee215aa50c1540eab31da73fc39ac13eed
HBSPS <wiyeong79@naver.com>
2022-08-07 16:22:04 +0900

    :sparkles: Themes
    ThemeProvider 연습

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import styled, { keyframes, Keyframes } from "styled-components";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -6,48 +10,13 @@ const Wrapper = styled.div`
   width: 100vw;
   justify-content: center;
   align-items: center;
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const rotateAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  } 50% {
-    border-radius: 100px;
-  } 100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotateAnimation} 1s linear infinite;
-  ${Emoji} {
-    &:hover {
-      font-size: 100px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😊</Emoji>
-      </Box>
+      <Title>Hello</Title>
     </Wrapper>
   )
 };
