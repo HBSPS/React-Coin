6f5fa82cafc8a126a64929936e8eb863b8105825
HBSPS <wiyeong79@naver.com>
2022-08-07 14:24:51 +0900

    :sparkles: Add animation & use Pseudo Selectors
    styled components의 애니메이션 만들기 + & 사용하기

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import styled from "styled-components";
+import styled, { keyframes, Keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  } 50% {
+    border-radius: 100px;
+  } 100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
   background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotateAnimation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 40px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>😊</span>
+      </Box>
+    </Wrapper>
   )
 };
 
