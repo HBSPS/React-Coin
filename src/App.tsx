294f55c09b6de8dde54ac99db61f7660195e524a
HBSPS <wiyeong79@naver.com>
2022-08-08 23:36:02 +0900

    :tada: CRYPTO TRACKER: SetUp
    React 프로젝트 준비 + 프로젝트 기본 구성 + useParams Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
+import Router from "./Router";
 
 function App() {
   return (
-    <Container>
-      <H1>Hello World!</H1>
-    </Container>
-  )
+    <Router />
+  );
 };
 
 export default App;
