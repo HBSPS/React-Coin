1c33242ff75a53af31cc0f031e66f0180b5ebefc
HBSPS <wiyeong79@naver.com>
2022-08-13 18:01:05 +0900

    :sparkles: ReactQueryDevtools
    ReactQueryDevtools 사용 및 useQuery에서 값이 중복되는 경우

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,21 @@
+const BASE_URL = `https://api.coinpaprika.com/v1`;
+
 export async function fetchCoins() {
-    const response = await fetch("https://api.coinpaprika.com/v1/coins");
+    const response = await fetch(`${BASE_URL}/coins`);
+    const json = await response.json();
+
+    return json;
+};
+
+export async function fetchCoinInfo(coinId: string) {
+    const response = await fetch(`${BASE_URL}/coins/${coinId}`);
+    const json = await response.json();
+
+    return json;
+};
+
+export async function fetchCoinTickers(coinId: string) {
+    const response = await fetch(`${BASE_URL}/tickers/${coinId}`);
     const json = await response.json();
 
     return json;
--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { Routes, Route, useLocation, useParams, Link, useMatch } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoinInfo, fetchCoinTickers } from "../api";
 
 import Chart from "./Chart";
 import Price from "./Price";
@@ -76,8 +78,8 @@ const Tab = styled.span<{ isActive: boolean }>`
   }
 `;
 
-interface RouteParams {
-    [coinId: string]: string;
+type RouteParams = {
+    coinId: string;
 };
 
 interface RouteState {
@@ -142,13 +144,16 @@ interface PriceData {
 };
 
 function Coin() {
-    const [loading, setLoading] = useState(true);
-    const { coinId } = useParams<RouteParams>();
+    const { coinId } = useParams() as RouteParams;
     const { state } = useLocation() as RouteState;
-    const [info, setInfo] = useState<InfoData>();
-    const [priceInfo, setPriceInfo] = useState<PriceData>();
     const priceMatch = useMatch("/:coinId/price");
     const chartMatch = useMatch("/:coinId/chart");
+    const {isLoading: infoLoading, data: infoData} = useQuery<InfoData>(["info", coinId], () => fetchCoinInfo(coinId));
+    const {isLoading: tickersLoading, data: tickersData} = useQuery<PriceData>(["tickers", coinId], () => fetchCoinTickers(coinId));
+    /* 
+    const [loading, setLoading] = useState(true);
+    const [info, setInfo] = useState<InfoData>();
+    const [priceInfo, setPriceInfo] = useState<PriceData>();
     useEffect(() => {
         (async () => {
             const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
@@ -157,12 +162,14 @@ function Coin() {
             setPriceInfo(priceData);
             setLoading(false);
         })();
-    }, [coinId])
+    }, [coinId]); 
+    */
+    const loading = infoLoading || tickersLoading;
     return (
         <Container>
             <Header>
                 <Title>
-                    {state?.name ? state.name : loading ? "Loading..." : info?.name}
+                    {state?.name ? state.name : loading ? "Loading..." : infoData?.name}
                 </Title>
             </Header>
             {loading ? (
@@ -172,26 +179,26 @@ function Coin() {
                     <Overview>
                         <OverviewItem>
                             <span>Rank:</span>
-                            <span>{info?.rank}</span>
+                            <span>{infoData?.rank}</span>
                         </OverviewItem>
                         <OverviewItem>
                             <span>Symbol:</span>
-                            <span>${info?.symbol}</span>
+                            <span>${infoData?.symbol}</span>
                         </OverviewItem>
                         <OverviewItem>
                             <span>Open Source:</span>
-                            <span>{info?.open_source ? "Yes" : "No"}</span>
+                            <span>{infoData?.open_source ? "Yes" : "No"}</span>
                         </OverviewItem>
                     </Overview>
-                    <Description>{info?.description}</Description>
+                    <Description>{infoData?.description}</Description>
                     <Overview>
                         <OverviewItem>
                             <span>Total Suply:</span>
-                            <span>{priceInfo?.total_supply}</span>
+                            <span>{tickersData?.total_supply}</span>
                         </OverviewItem>
                         <OverviewItem>
                             <span>Max Supply:</span>
-                            <span>{priceInfo?.max_supply}</span>
+                            <span>{tickersData?.max_supply}</span>
                         </OverviewItem>
                     </Overview>
                         
